36f1eb4da2cc3f19e1886fc3b3b0a1bb97a3e3ad
sooryasrajan <soorya.s27@gmail.com>
2022-06-11 16:06:12 +0530

    Added login page and signup page

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_socket_chatapp/screens/home_page.dart';
+import 'package:flutter_socket_chatapp/screens/signup_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -10,6 +12,67 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(child: Container()),
+            const Text(
+              "Login",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    labelText: "User Name",
+                    hintText: "Enter user name, eg: john_myers",
+                    filled: true,
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10, bottom: 10),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    labelText: "Password",
+                    hintText: "Enter your password here",
+                    filled: true,
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignUpPage()));
+                    },
+                    child: const Text("Don't have an account? Sign up"))),
+            Expanded(child: Container()),
+            Align(
+              alignment: Alignment.centerRight,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomePage()),
+                      (Route<dynamic> route) => false);
+                },
+                child: const Text(
+                  'Sign In',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_socket_chatapp/screens/home_page.dart';
+import 'package:flutter_socket_chatapp/screens/login_page.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
@@ -10,6 +12,77 @@ class SignUpPage extends StatefulWidget {
 class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(child: Container()),
+            const Text(
+              "Sign Up",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    labelText: "User Name",
+                    hintText: "Enter user name, eg: john_myers",
+                    filled: true,
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    labelText: "Profile Name",
+                    hintText: "Enter profile name, eg: john_myers",
+                    filled: true,
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10, bottom: 10),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    labelText: "Password",
+                    hintText: "enter your password here",
+                    filled: true,
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginPage()));
+                    },
+                    child: const Text("Already have an account? Login"))),
+            Expanded(child: Container()),
+            Align(
+              alignment: Alignment.centerRight,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomePage()),
+                      (Route<dynamic> route) => false);
+                },
+                child: const Text(
+                  'Sign Up',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
